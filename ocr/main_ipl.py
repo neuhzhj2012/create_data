09fc8db3bace24d1177665ee5481324978c148c6
hzhj2012 <huangzhongjie@autohome.com.cn>
2020-05-29 20:27:58 +0800

    解决字符越界和重叠问题

--- ocr/main_ipl.py
+++ ocr/main_ipl.py
@@ -1,5 +1,6 @@
 #encoding: utf-8
 import os
+import uuid
 import random
 from load_ttf import MYFONT
 from get_content import CONT
@@ -15,19 +16,28 @@ if __name__ == '__main__':
 
     num_create = 5 #总的图片数
     num_conts = 7 #单张图片上文字区域
+    width_char = 30#单字符的宽度
 
 
     for _ in list(range(num_create)):
         #需要区分是否可重叠
-        w_list = random.sample(list(range(imgwh[0])), num_conts ) #起点位置
-        h_list = random.sample(list(range(imgwh[1])), num_conts )
+        # w_list = random.sample(list(range(0, imgwh[0]-width_char)), num_conts ) #起点位置
+        # h_list = random.sample(list(range(imgwh[1])), num_conts ) #造成垂直方向上字符重叠
 
+        w_list=random.sample(list(range(int(imgwh[0] / width_char) - 1)), num_conts)
+        w_list = [k * width_char for k in w_list]
+
+        h_list=random.sample(list(range(int(imgwh[1] / width_char) - 1)), num_conts)
+        h_list = [k * width_char for k in h_list]
+
+
+        img_pil = fontObj.getAlphaNewImage(imgwh)
         for x, y in zip(w_list, h_list):
-            w_cont = random.sample(list(range(x, imgwh[0])), 1)[0] #当前文本的宽高信息
+            w_cont = random.sample(list(range(x, imgwh[0]-width_char)), 1)[0] #当前文本的宽高信息
             h_cont = random.sample(h_txt_range, 1)[0]
             txt_font = random.sample(fonts, 1)[0]  #字体选择
 
-            num_total = int(w_cont/3)
+            num_total = int(w_cont/width_char)
             num_chinese = int(num_total/2)
             num_alpha = int(num_total/5)
             num_num = num_total - num_chinese - num_alpha
@@ -37,9 +47,17 @@ if __name__ == '__main__':
             if len(txt) == 8:
                 fontObj.setFontSize(txt_font, 20)
             txt_cord = fontObj.getTxtCord(txt, txt_font)
-            img_pil = fontObj.getTxtPartBase(imgwh, txt, txt_cord, txt_font)
+            if (imgwh[0] - x - txt_cord[2] < 0): #需要换行,此时修改起始坐标
+                txt_cord = (txt_cord[0] + imgwh[0] - txt_cord[2], txt_cord[1] + y, txt_cord[2], txt_cord[3])
+            else:
+                txt_cord = (txt_cord[0] + x, txt_cord[1] + y, txt_cord[2], txt_cord[3])
+            # txt_cord = (txt_cord[0] + x, txt_cord[1] + y, min(txt_cord[2], imgwh[0] - x), txt_cord[3]) #截断文字
+            print(txt_cord, txt_cord[0]+txt_cord[2], len(txt), txt)
+
+            img_pil = fontObj.getTxtPartBase(img_pil, txt, txt_cord, txt_font)
+            # img_pil.save(os.path.join(dst_folder, str(uuid.uuid4()) + '.jpg'))
             # img_pil.show()
-            img_pil.save(os.path.join(dst_folder, txt + '.jpg'))
+        img_pil.save(os.path.join(dst_folder, str(uuid.uuid4()) + '.jpg'))
 
     # for province in provinces: #当前省份内数据扩大10倍
     #     for alpha in contObj.get_province_altha(province) * 50: #每个城市字母下生成500个
